8f6a9b5c52edf780715ceea9db01e33bd8de8355
rhiokim <rhio.kim@gmail.com>
2013-02-21 13:19:26 +0900

    bump

--- src/js/app/editor/Editor.js
+++ src/js/app/editor/Editor.js
@@ -1,17 +1,28 @@
 define([
-		'module'
+		'module',
+		'keyboard'
 	],
-	function(module) {
-		
-		module.exports = CodeMirror.fromTextArea(document.getElementById("code"), {
+	function(module, HotKey) {
+		var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 					    mode: 'markdown',
 					    lineNumbers: true,
 					    theme: "solarized dark",
-					    keyMap: "vim",
+					    // keyMap: "vim",
 					    viewportMargin: 40,
 					    lineWrapping: true,
 					    autofocus: true,
 					    extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
 					  });
+
+
+		HotKey('super-ctrl-l', function() {
+			alert('')
+		});
+		HotKey('shift-ctrl-v', function() {
+			var map = editor.getOption('keyMap');
+			alert(map);
+			editor.setOption(map == 'vim' ? '' : 'vim');
+		});
 		
+		module.exports = editor;
 });
