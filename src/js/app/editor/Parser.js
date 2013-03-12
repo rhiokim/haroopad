6d8d6201aba63920fe88ee92a27ae3bdb4e0cd3e
rhiokim <rhio.kim@gmail.com>
2013-03-12 11:56:25 +0900

    test menubar, but not support node-webkit

--- src/js/app/editor/Parser.js
+++ src/js/app/editor/Parser.js
@@ -4,34 +4,34 @@ define([
 	],
 	function(module, HotKey) {
 		marked.setOptions({
-          gfm: true,
-          tables: true,
-          breaks: false,
-          pedantic: false,
-          sanitize: true,
-          smartLists: true,
-          langPrefix: '',
-          highlight: function(code, lang) {
-            var res;
-            if(!lang) {
-              return code;
-            }
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      langPrefix: '',
+      highlight: function(code, lang) {
+        var res;
+        if(!lang) {
+          return code;
+        }
 
-            switch(lang) {
-              case 'js':
-                lang = 'javascript';
-              break;
-            }
+        switch(lang) {
+          case 'js':
+            lang = 'javascript';
+          break;
+        }
 
-            try {
-              res = hljs.highlight(lang, code).value;
-            } catch(e) {
+        try {
+          res = hljs.highlight(lang, code).value;
+        } catch(e) {
 
-            } finally {
-              return res || code;
-            }
-          }
-        });
+        } finally {
+          return res || code;
+        }
+      }
+    });
 
 		module.exports = marked;
 });
