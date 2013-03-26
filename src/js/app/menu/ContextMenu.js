8020e74cb5ca1d909fd9bc9b527bc20fc3b30ade
rhiokim <rhio.kim@gmail.com>
2013-03-26 14:55:07 +0900

    contextmenu sync with viewer

--- src/js/app/menu/ContextMenu.js
+++ src/js/app/menu/ContextMenu.js
@@ -6,17 +6,25 @@ define([
 		var menu = new gui.Menu();
 
 		// Add some items
-		menu.append(new gui.MenuItem({ label: 'Save' }));
-		menu.append(new gui.MenuItem({ label: 'Item B' }));
+		menu.append(new gui.MenuItem({ label: 'Cut' }));
+		menu.append(new gui.MenuItem({ label: 'Copy' }));
+		menu.append(new gui.MenuItem({ label: 'Paste' }));
 		menu.append(new gui.MenuItem({ type: 'separator' }));
-		menu.append(new gui.MenuItem({ label: 'Item C' }));
+		menu.append(new gui.MenuItem({ label: 'Search with Google' }));
 		menu.append(new gui.MenuItem({ type: 'separator' }));
 		menu.append(new gui.MenuItem({ label: 'Preferences' }));
 
-		$(document.body).bind('contextmenu', function(ev) { 
-			ev.preventDefault();
+		$(window).mousedown(function(e, ev) { 
+			var x, y;
+			e.preventDefault();
 
-			menu.popup(ev.pageX, ev.pageY);
-			return false;
+			e = (ev) ? ev : e;
+			x = (ev) ? $('#editor').width() + e.pageX : e.pageX;
+
+			if (e.which === 3) {
+				menu.popup(x, e.pageY);
+				return false;
+	    }
+
 		});
 });
