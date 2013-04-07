2735ad4c7302a44176dae49844a3d24f2232298f
rhiokim <rhio.kim@gmail.com>
2013-04-08 02:59:31 +0900

    add markdown syntax & help menu action

--- src/js/app/window/Window.help.js
+++ src/js/app/window/Window.help.js
@@ -5,8 +5,21 @@ define(function() {
 
 	win.on('help', function() {});
 	win.on('help.markdown', function() {});
-	win.on('help.release', function() {});
-	win.on('help.acknowledgements', function() {});
-	win.on('help.site', function() {});
-	win.on('help.feedback', function() {});
+	win.on('help.release', function() {
+		shell.openExternal('http://pad.haroopress.com/release');
+	});
+	win.on('help.acknowledgements', function() {
+  	gui.Window.open('pad.html', {
+			width: 350,
+			height: 500,
+		  toolbar: false,
+		  show: false
+		});
+	});
+	win.on('help.site', function() {
+		shell.openExternal('http://pad.haroopress.com/');
+	});
+	win.on('help.feedback', function() {
+		shell.openExternal('http://pad.haroopress.com/feedback');
+	});
 });
