2dc4bc3c4f19aac0abbd5fe4ac7462ec3d681cf0
rhiokim <rhio.kim@gmail.com>
2013-04-08 16:17:53 +0900

    add about.md

--- src/js/app/window/Window.help.js
+++ src/js/app/window/Window.help.js
@@ -4,21 +4,26 @@ define(function() {
 	var shell = gui.Shell;
 
 	win.on('help', function() {});
+	
 	win.on('help.markdown', function() {});
+
 	win.on('help.release', function() {
 		shell.openExternal('http://pad.haroopress.com/release');
 	});
+
 	win.on('help.acknowledgements', function() {
-  	gui.Window.open('pad.html', {
+  		gui.Window.open('pad.html', {
 			width: 350,
 			height: 500,
-		  toolbar: false,
-		  show: false
+			toolbar: false,
+			show: false
 		});
 	});
+
 	win.on('help.site', function() {
 		shell.openExternal('http://pad.haroopress.com/');
 	});
+
 	win.on('help.feedback', function() {
 		shell.openExternal('http://pad.haroopress.com/feedback');
 	});
