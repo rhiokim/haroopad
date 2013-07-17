073bfd1af36429a9a16bb3227650c68041046ce4
rhiokim <rhio.kim@gmail.com>
2013-07-17 21:47:15 +0900

    add starrre plugin

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,16 +1,67 @@
-// Some general UI pack related JS
+function setStarrre() {
+	$('#shareme').attr('data-url', window.location.href);
+  $('#shareme').sharrre({
+	  share: {
+	    googlePlus: true,
+	    facebook: true,
+	    twitter: true,
+	    digg: true,
+	    delicious: true,
+	    stumbleupon: true,
+	    linkedin: true/*,
+	    pinterest: true*/
+	  },
+	  buttons: {
+	    googlePlus: {size: 'tall', annotation:'bubble'},
+	    facebook: {layout: 'box_count'},
+	    twitter: {count: 'vertical'},
+	    digg: {type: 'DiggMedium'},
+	    delicious: {size: 'tall'},
+	    stumbleupon: {layout: '5'},
+	    linkedin: {counter: 'top'}/*,
+	    pinterest: {media: 'http://sharrre.com/img/example1.png', description: $('#shareme').data('text'), layout: 'vertical'}*/
+	  },
+	  enableHover: false,
+	  enableCounter: false,
+	  enableTracking: true
+	});
+}
+
+function loadPost(file) {
+  var dir = 'docs/'+ file.replace('#', '') +'/';
+  file = dir +'index.md';
+
+  $.ajax({
+    url: file
+  }).done(function(data) {
+    var str = marked(data);
+    str = str.replace(/src=\"images/g, 'src="'+ dir +'images');
+    $('.contents').html(str);
+
+
+  	setStarrre();
+  });
+}
 
 $(document).ready(function() {
-    // Todo list
-    $(".todo li").click(function() {
-        $(".todo li").removeClass('todo-done');
-        $(this).addClass("todo-done");
-    });
+  var file;
 
-    // Init tooltips
-    $("[data-toggle=tooltip]").tooltip({ html: true });
+  $('.share a').click(function(e) {
+    file = $(this).attr('href');
 
-    $('#slider').nivoSlider();
+    if(file.indexOf('#') < 0) {
+      e.preventDefault();
+      return;
+    } else {
+      loadPost(file);
+    }
+  });
 
+  file = url('#');
+
+  if(file) {
+    loadPost(file);
+  } else {
+    loadPost('about');
+  }
 });
-
