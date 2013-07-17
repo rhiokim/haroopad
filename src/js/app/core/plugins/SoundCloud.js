ae077185a7a9ed182129911e87770b327dcedc3d
rhiokim <rhio.kim@gmail.com>
2013-07-18 00:30:34 +0900

    update soundcloud plugin

--- src/js/app/core/plugins/SoundCloud.js
+++ src/js/app/core/plugins/SoundCloud.js
@@ -9,17 +9,21 @@ define(function() {
 	    .replace(/'/g, '&#39;');
 	}
 
-	return function(args) {
-		var _width = '100%';
-		var _height = '166';
+	var _width = '100%';
+	var _height = '166';
+	var _auto = false;
+	var _artwork = false;
 
+	return function(args) {
 		args = args.split(' ');
 		key = args[0];
 		width = args[1] || _width;
 		height = args[2] || _height;
+		auto = args[3] || _auto;
+		artwork = args[4] || _artwork;
 
 		// var real = '<iframe class="youtube" width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/' + key + '" frameborder="0" allowfullscreen></iframe>';
-		var real = '<iframe width="'+ width +'" height="'+ height +'" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F'+ key +'"></iframe>';
+		var real = '<iframe width="'+ width +'" height="'+ height +'" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F'+ key +'&amp;auto_play='+ auto +'&amp;show_artwork='+ artwork +'"></iframe>';
 		real = escape(real);
 		return '<img src="img/plugins/youtube.jpg" width="'+width+'" height="'+height+'" origin="'+ real +'" class="plugin" />';
 	}
