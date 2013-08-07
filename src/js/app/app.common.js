09180b9ddc0877fccb2c9b8b044efbad1721e956
rhiokim <rhio.kim@gmail.com>
2013-08-07 22:34:49 +0900

    remove ad bgcolor

--- src/js/app/app.common.js
+++ src/js/app/app.common.js
@@ -2,10 +2,10 @@ var _glo = {
 	getEmailAdvertisementHTML: function() {
 		return ''
 			+ '<hr>'
-			+ '<p style="display: block; width: 99.6%; text-align: center; padding: 5px 0; font-size:1em; border: 1px solid #e5e5e5; background-color:rgb(253,246,227); border-radius: 3px;">'
+			+ '<p style="display: block; width: 99.6%; text-align: center; padding: 5px 0; font-size:1em; border: 1px solid #e5e5e5;/* background-color:rgb(253,246,227);*/ border-radius: 3px;">'
 			+ '	<span>'
-			+ '   <img src="http://pad.haroopress.com/assets/images/logo-small.png" align="absmiddle" width="42" height="42" style="margin-right: 10px;"/>'
-			+ '   AD : The next document processor <a href="http://pad.haroopress.com/" target="_blank">Haroopad</a>, '
+			+ '   <img src="http://pad.haroopress.com/assets/images/logo-small.png" align="absmiddle" width="32" height="32" style="margin-right: 10px;"/>'
+			+ '   ad : The Next Document processor based on Markdown <a href="http://pad.haroopress.com/" target="_blank">Haroopad</a>, '
  			+ '		<a href="http://pad.haroopress.com/user.html#download" target="_blank">Download</a>'
 			+ ' </span>'
 			+ '</p>';
@@ -15,7 +15,7 @@ var _glo = {
 		return ''
 			+ '\n----------------------  \n'
 			+ '\t> **AD**  \n'
-			+ '\t> This e-mail is mail that is easily created in Haroopad.  \n'
+			+ '\t> Haroopad - The Next Document processor based on Markdown  \n'
 			+ '\t> [Download Haroopad](http://pad.haroopress.com/user.html#download)  \n';
 	}
 }
