cfd96284412b4c1f0b62e8e993990a9b3fd242fe
rhiokim <rhio.kim@gmail.com>
2013-08-08 14:46:08 +0900

    fixed adjust theme error when send email

--- src/js/pad/viewer/Viewer.explicitStyleMaker.js
+++ src/js/pad/viewer/Viewer.explicitStyleMaker.js
@@ -1,136 +1,158 @@
 define([
-		// 'ui/file/File.opt',
-		],function(/*FileOpt*/) {
+	// 'ui/file/File.opt',
+], function( /*FileOpt*/ ) {
 
-		var fs = require('fs'),
-				path = require('path');
+	var fs = require('fs'),
+		path = require('path');
 
-		var iframe = $('#haroo iframe')[0];
-		var viewer = iframe.contentWindow;
+	var iframe = $('#haroo iframe')[0];
+	var viewer = iframe.contentWindow;
 
-		var htmlDoc = iframe.contentDocument;
-		var htmlStyledDoc;
+	var htmlDoc = iframe.contentDocument;
+	var htmlStyledDoc;
 
-		function setInlineStyles(rules) {
-			var i, j, selectorMatches, styleAttr;
-			
-			for (i = 0; i < rules.length; i++) {
-				rule = rules[i];
+	function setInlineStyles(rules) {
+		var i, j, selectorMatches, styleAttr;
 
-				selectorMatches = htmlStyledDoc.querySelectorAll(rule.selectorText);
+		for (i = 0; i < rules.length; i++) {
+			rule = rules[i];
 
-				for (j = 0; j < selectorMatches.length; j++ ) {
-					elem = selectorMatches[j];
-					styleAttr = selectorMatches[j].getAttribute('style') || '';
+			// if (rule.selectorText === 'body') {
+			// 	styleAttr = htmlStyledDoc.getAttribute('style') || '';
 
-					if (styleAttr && styleAttr.search(/;[\s]*$/) < 0) {
-	          styleAttr += '; ';
-	        }
+			// 	if (styleAttr && styleAttr.search(/;[\s]*$/) < 0) {
+			// 		styleAttr += '; ';
+			// 	}
 
-	        styleAttr += rule.style.cssText;
+			// 	styleAttr += rule.style.cssText;
 
-	        selectorMatches[j].setAttribute('style', styleAttr);
+			// 	htmlStyledDoc.setAttribute('style', styleAttr);
+			// 	continue;
+			// }
+
+			selectorMatches = htmlStyledDoc.querySelectorAll(rule.selectorText);
+
+			for (j = 0; j < selectorMatches.length; j++) {
+				elem = selectorMatches[j];
+				styleAttr = selectorMatches[j].getAttribute('style') || '';
+
+				if (styleAttr && styleAttr.search(/;[\s]*$/) < 0) {
+					styleAttr += '; ';
 				}
+
+				styleAttr += rule.style.cssText;
+
+				selectorMatches[j].setAttribute('style', styleAttr);
+			}
+		}
+	}
+
+	function makeStylesExplicit() {
+		var styleSheets, i;
+
+		styleSheets = htmlDoc.styleSheets;
+
+		for (i = 0; i < styleSheets.length; i++) {
+			setInlineStyles(styleSheets[i].cssRules);
+		}
+
+		var wrapper = document.createElement('div');
+		wrapper.innerHTML = htmlStyledDoc.lastElementChild.innerHTML;
+		wrapper.setAttribute('style', htmlStyledDoc.lastElementChild.getAttribute('style'));
+
+		// var wrapper = $('<div>').html(htmlStyledDoc.lastElementChild.innerHTML);
+		// wrapper.attr('style', $(htmlStyledDoc.lastElementChild).attr('style'));
+
+		// FileOpt.set('html', wrapper[0].outerHTML);
+
+		// nw.file.set('html', wrapper[0].outerHTML);
+		nw.file.set({ html: wrapper.outerHTML}, { silent: true });
+	}
+
+	function makeImageEncode() {
+		var i, img, image, images;
+
+		images = htmlDoc.querySelectorAll('img');
+
+		for (i = 0; i < images.length; i++) {
+			image = images[i];
+			src = image.src;
+
+			if (!src) {
+				continue;
+			}
+
+			if (process.platform !== 'win32') {
+				src = src.replace('file://', '');
+			}
+
+			if (src.indexOf('://') < 0) {
+				src = decodeURIComponent(src);
+				img = fs.readFileSync(src);
+
+				// image.src = 'data:image/png;base64,'+ img.toString('base64');
+				// image.src = 'data:image/png;base64,';
+			}
+		}
+	}
+
+	function attachImage() {
+		var i, name, img, image, images;
+		var attachments = [];
+
+		images = htmlStyledDoc.querySelectorAll('img');
+
+		for (i = 0; i < images.length; i++) {
+			image = images[i];
+			src = image.src;
+
+			if (!src) {
+				continue;
+			}
+
+			if (process.platform !== 'win32') {
+				src = src.replace('file://', '');
+			}
+
+			if (src.indexOf('://') < 0) {
+				src = decodeURIComponent(src);
+				src = path.normalize(src);
+				name = path.basename(src);
+
+				// src = src.replace(' ', '\ ');
+				attachments.push({
+					fileName: name,
+					// streamSource: fs.createReadStream(src)
+					// contentType: "image/png"
+					filePath: src,
+					cid: name
+				});
+
+				// image.src = 'data:image/png;base64,'+ img.toString('base64');
+				// image.src = 'data:image/png;base64,';
+				image.src = 'cid:' + name;
 			}
 		}
 
-		function makeStylesExplicit() {
-		  var styleSheets, i;
+		// attachments.push(
+		//     {   // use URL as an attachment
+		//         fileName: "license.txt",
+		//         filePath: "https://raw.github.com/andris9/Nodemailer/master/LICENSE"
+		//     });
 
-		  styleSheets = htmlDoc.styleSheets;
+		// FileOpt.set('attachments', attachments);
+		nw.file.set('attachments', attachments);
+	}
 
-		  for (i = 0; i < styleSheets.length; i++) {
-		  	setInlineStyles(styleSheets[i].cssRules);
-		  }
+	window.ee.on('menu.file.send.email', function() {
 
-		  var wrapper = $('<div>').html(htmlStyledDoc.innerHTML);
-		  wrapper.attr('style', $(htmlStyledDoc.body).attr('style'));
+		// htmlStyledDoc = $(htmlDoc.documentElement).clone()[0];
+		htmlStyledDoc = document.createElement('html');
+		htmlStyledDoc.innerHTML = htmlDoc.documentElement.innerHTML;
+		// htmlStyledDoc = $('<html>').html(htmlDoc.documentElement.innerHTML)[0];
 
-		  // FileOpt.set('html', wrapper[0].outerHTML);
-		  nw.file.set('html', wrapper[0].outerHTML);
-	  }
-
-	  function makeImageEncode() {
-	  	var i, img, image, images;
-
-	  	images = htmlDoc.querySelectorAll('img');
-
-	  	for (i = 0; i < images.length; i++) {
-	  		image = images[i];
-	  		src = image.src;
-
-	  		if(!src) {
-	  			continue;
-	  		}
-				
-				if (process.platform !== 'win32') {
-					src = src.replace('file://', '');
-				}
-	  		
-	  		if(src.indexOf('://') < 0) {
-	  			src = decodeURIComponent(src);
-	  			img = fs.readFileSync(src);
-
-	  			// image.src = 'data:image/png;base64,'+ img.toString('base64');
-	  			// image.src = 'data:image/png;base64,';
-	  		}
-	  	}
-	  }
-
-	  function attachImage() {
-	  	var i, name, img, image, images;
-	  	var attachments = [];
-
-	  	images = htmlStyledDoc.querySelectorAll('img');
-
-	  	for (i = 0; i < images.length; i++) {
-	  		image = images[i];
-	  		src = image.src;
-
-	  		if(!src) {
-	  			continue;
-	  		}
-				
-				if (process.platform !== 'win32') {
-					src = src.replace('file://', '');
-				}
-	  		
-	  		if(src.indexOf('://') < 0) {
-	  			src = decodeURIComponent(src);
-	  			src = path.normalize(src);
-	  			name = path.basename(src);
-
-	  			// src = src.replace(' ', '\ ');
-	  			attachments.push({
-	  				fileName: name,
-	  				// streamSource: fs.createReadStream(src)
-	  				// contentType: "image/png"
-	  				filePath: src,
-	  				cid: name
-	  			});
-
-	  			// image.src = 'data:image/png;base64,'+ img.toString('base64');
-	  			// image.src = 'data:image/png;base64,';
-	  			image.src = 'cid:'+ name;
-	  		}
-	  	}
-
-	  	// attachments.push(
-    //     {   // use URL as an attachment
-    //         fileName: "license.txt",
-    //         filePath: "https://raw.github.com/andris9/Nodemailer/master/LICENSE"
-    //     });
-
-	  	// FileOpt.set('attachments', attachments);
-	  	nw.file.set('attachments', attachments);
-	  }
-
-		window.ee.on('menu.file.send.email', function() {
-
-			htmlStyledDoc = $(htmlDoc.body).clone()[0];
-			
-			// makeImageEncode();
-			attachImage();
-			makeStylesExplicit();
-		});
+		// makeImageEncode();
+		attachImage();
+		makeStylesExplicit();
+	});
 });
