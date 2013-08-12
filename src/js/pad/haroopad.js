a30b83857b4d8077b791eb172b453066133bc7f4
rhiokim <rhio.kim@gmail.com>
2013-08-12 20:28:05 +0900

    1st complet header

--- src/js/pad/haroopad.js
+++ src/js/pad/haroopad.js
@@ -188,4 +188,21 @@ requirejs([
         break;
       }
     });
+
+
+    /* control gui editor */
+    $('#editControls a').click(function(e) {
+      switch($(this).data('role')) {
+        case 'h1':
+        case 'h2':
+        case 'p':
+          document.execCommand('formatBlock', false, '<' + $(this).data('role') + '>');
+          break;
+        default:
+          document.execCommand($(this).data('role'), false, null);
+         break;
+     }
+     
+   })
+
 });
