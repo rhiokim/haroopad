6879c18aa151bc90fdfce4c34e15870bb6072382
rhiokim <rhio.kim@gmail.com>
2013-08-15 13:07:27 +0900

    update ad message

--- src/js/app/app.common.js
+++ src/js/app/app.common.js
@@ -1,12 +1,13 @@
 var _glo = {
 	getEmailAdvertisementHTML: function() {
 		return ''
-			+ '<hr>'
-			+ '<p style="display: block; width: 99.6%; text-align: center; padding: 5px 0; font-size:1em; border: 1px solid #e5e5e5;/* background-color:rgb(253,246,227);*/ border-radius: 3px;">'
+			// + '<hr>'
+			+ '<p style="display: block; width: 99.6%; line-height:1.6em; /*text-align: center; */padding: 5px 0; font-size:1em; border: 1px solid #e5e5e5;/* background-color:rgb(253,246,227);*/ border-radius: 3px;">'
 			+ '	<span>'
-			+ '   <img src="http://pad.haroopress.com/assets/images/logo-small.png" align="absmiddle" width="32" height="32" style="margin-right: 10px;"/>'
-			+ '   Tip : The Next Document processor based on Markdown <a href="http://pad.haroopress.com/" target="_blank">Haroopad</a>, '
- 			+ '		<a href="http://pad.haroopress.com/user.html#download" target="_blank">Download</a>'
+			+ '   <img src="http://pad.haroopress.com/assets/images/logo-small.png" align="absmiddle" width="40" height="40" style="float:left; margin:0 10px;"/>'
+			+ '   Sent from My <strong><a href="http://pad.haroopress.com/" target="_blank">Haroopad</a></strong> <br/>'
+			+ '   The Next Document processor based on Markdown '
+ 			+ '	- <a href="http://pad.haroopress.com/user.html#download" target="_blank">Download</a>'
 			+ ' </span>'
 			+ '</p>';
 	},
@@ -14,8 +15,8 @@ var _glo = {
 	getEmailAdvertisementMD: function() {
 		return ''
 			+ '\n* * * * * * * *  \n'
-			+ '\t> **Tip**  \n'
-			+ '\t> Haroopad - The Next Document processor based on Markdown  \n'
+			+ '\t> Sent from My **[Haroopad](http://pad.haroopress.com)**  \n'
+			+ '\t> The Next Document processor based on Markdown  \n'
 			+ '\t> [Download Haroopad](http://pad.haroopress.com/user.html#download)  \n';
 	}
 }
