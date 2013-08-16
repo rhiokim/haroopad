da2ff3dfd675823a1f52b89cd6f67d82110441cd
rhiokim <rhio.kim@gmail.com>
2013-08-16 12:51:10 +0900

    fixed open file in leaved fullscreen

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,7 +359,7 @@ module.exports = function(grunt) {
       },
 
       deploy: {
-        command: 'cp -R ./build/haroopad.app /Applications'
+        command: 'rm -rf /Applications/haroopad.app; cp -R ./build/haroopad.app /Applications'
       }
     },
 
