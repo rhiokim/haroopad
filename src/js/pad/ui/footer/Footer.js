351ee2501f692cc38b18186e2b31f66eb053518e
rhiokim <rhio.kim@gmail.com>
2013-08-17 18:02:04 +0900

    word, line count & more detail info

--- src/js/pad/ui/footer/Footer.js
+++ src/js/pad/ui/footer/Footer.js
@@ -2,6 +2,8 @@ define([
 	'editor/Editor'
 	], function(Editor) {
 
+		var humanize = require('humanize');
+
 	var Footer = Backbone.View.extend({
 		el: 'footer .navbar-inner',
 
@@ -15,16 +17,33 @@ define([
 
 			Editor.on('update', this.updateHandler.bind(this));
 			Editor.on('cursorActivity', this.cursorActivity.bind(this));
+
+			this.$('#elementsInfo').tooltip({
+				html: true,
+				title: 'header: 43<br/>'
+						+ 'boild: 14<br/>'
+						+ 'italic: 3<br/>'
+						+ 'blockquote: 3<br/>'
+						+ 'img: 4<br/>'
+						+ 'paragraph: 332<br/>'
+						+ 'link: 72<br/>'
+						+ 'table: 3<br/>'
+						+ 'code: 8'
+			})
 		},
 
 		updateHandler: function(cm) {
-			this.lineCount.text(cm.lineCount());
-			this.wordCount.text(cm.getValue().length);
+			var line = humanize.numberFormat(cm.lineCount(), 0);
+			var word = humanize.numberFormat(cm.getValue().length, 0);
+			this.lineCount.text(line);
+			this.wordCount.text(word);
 		},
 
 		cursorActivity: function(cm) {
 			var pos = cm.getCursor();
-			this.cursorPos.text(pos.line + ':' + pos.ch);
+			var line = humanize.numberFormat(pos.line, 0);
+			var word = humanize.numberFormat(pos.ch, 0);
+			this.cursorPos.text('('+ line + ' : ' + word +')');
 		}
 	});
 
