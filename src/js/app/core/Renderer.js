16e259502e6f7d0312736ca984a2d391eeee4282
rhiokim <rhio.kim@gmail.com>
2013-10-09 03:44:40 +0900

    update acknowledgements.md

--- src/js/app/core/Renderer.js
+++ src/js/app/core/Renderer.js
@@ -25,47 +25,11 @@ define([
 		// 	return plugin(name, args);
 		// }
 
-		// renderer.oembed = function(caption, href, props) {
-		// 	var key, value, tmp = {};
-
-		// 	if (!href) {
-		// 		return '';
-		// 	}
-
-		// 	props = !props ? '' : props ;
-
-		// 	if (props) {
-		// 		props = props.split(',');
-		// 		props.forEach(function(prop) {
-		// 			prop = prop.split(':');
-		// 			tmp[prop[0]] = prop[1];
-		// 		});
-		// 		props = JSON.stringify(tmp);
-		// 		props = encodeURIComponent(props);
-		// 	}
-		// 	return '<p href="'+ href +'" data-origin="'+ href +'#'+ props +'" data-props="'+ props +'" class="oembed">'+ loading +'</p>';
-		// }
-
-		renderer.image = function(cap, href, props) {
+		renderer.oembed = function(caption, href, props) {
 			var key, value, tmp = {};
-			var imgPattern = /[^\s]+(\.(jpg|png|gif|bmp|jpeg))$/i;
 
 			if (!href) {
 				return '';
-			}
-
-			if (imgPattern.test(href)) {
-			  return '<img src="'
-			      + href
-			      + '" alt="'
-			      + escape(cap[1])
-			      + '"'
-			      + (props
-			      ? ' title="'
-			      + escape(props)
-			      + '"'
-			      : '')
-			      + '>';
 			}
 
 			props = !props ? '' : props ;
@@ -82,8 +46,47 @@ define([
 				props = JSON.stringify(tmp);
 				props = encodeURIComponent(props);
 			}
-			return '<p data-url="'+ href +'" data-origin="'+ href +'#'+ props +'" data-props="'+ props +'" class="oembed">'+ loading +'</p>';
-		};
+			return '<p href="'+ href +'" data-origin="'+ href +'#'+ props +'" data-props="'+ props +'" class="oembed">'+ loading +'</p>';
+		}
+
+		// renderer.image = function(cap, href, props) {
+		// 	var key, value, tmp = {};
+		// 	var imgPattern = /[^\s]+(\.(jpg|png|gif|bmp|jpeg))$/i;
+
+		// 	if (!href) {
+		// 		return '';
+		// 	}
+
+		// 	if (imgPattern.test(href)) {
+		// 	  return '<img src="'
+		// 	      + href
+		// 	      + '" alt="'
+		// 	      + escape(cap[1])
+		// 	      + '"'
+		// 	      + (props
+		// 	      ? ' title="'
+		// 	      + escape(props)
+		// 	      + '"'
+		// 	      : '')
+		// 	      + '>';
+		// 	}
+
+		// 	props = !props ? '' : props ;
+
+		// 	if (props) {
+		// 		props = props.split(',');
+		// 		props.forEach(function(prop) {
+		// 			prop = prop.split(':');
+
+		// 			if (prop[0] && prop[1]) {
+		// 				tmp[prop[0].trim()] = prop[1].trim();
+		// 			}
+		// 		});
+		// 		props = JSON.stringify(tmp);
+		// 		props = encodeURIComponent(props);
+		// 	}
+		// 	return '<p data-url="'+ href +'" data-origin="'+ href +'#'+ props +'" data-props="'+ props +'" class="oembed">'+ loading +'</p>';
+		// };
 
 		return renderer;
 });
