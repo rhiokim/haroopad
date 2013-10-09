c57deb404bc99b5af1afbbc15a7d31e6b538f9a1
rhiokim <rhio.kim@gmail.com>
2013-10-10 04:12:19 +0900

    fixed export html with MathJax

--- src/js/pad/ui/exports/Exports.js
+++ src/js/pad/ui/exports/Exports.js
@@ -37,6 +37,12 @@ define([
 
 			_.each(contentDocument.styleSheets, function(item) {
 				href = item.href;
+
+				if (!href) {
+					cssText += item.innerHTML;
+					return;
+				}
+				
 				href = href.split('?')[0];
 				href = decodeURIComponent(href);
 				href = href.replace('file:///', '');
@@ -124,7 +130,7 @@ define([
 			save(file);
 
 			saveEl.off('change', saveHandler);
-			saveEl.val('');
+			saveEl.val("");
 		}
 
 		window.ee.on('file.exports.html', function() {
@@ -139,7 +145,7 @@ define([
 			// res = res.replace('@@author', os.hostname());
 
       		saveEl.attr('nwsaveas', title );
-			saveEl.trigger('click');
+			saveEl.trigger("click");
 			saveEl.on('change', saveHandler);
 			/**
 			 * 1. get html
