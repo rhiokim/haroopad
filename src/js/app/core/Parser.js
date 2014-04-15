c105ca781f315c62358d606b98f21a8cf765ebb5
rhiokim <rhio.kim@gmail.com>
2014-04-15 18:38:05 +0900

    fixed regen marked instance logic

--- src/js/app/core/Parser.js
+++ src/js/app/core/Parser.js
@@ -1,19 +1,35 @@
 define([
     'core/Lexer',
-    'core/InlineLexer'
+    'core/InlineLexer',
+    'core/Renderer'
   ],
-  function(Lexer, InlineLexer) {
+  function(Lexer, InlineLexer, Renderer) {
 
     var gui = require('nw.gui'),
         win = gui.Window.get();
 
     var marked = require("marked");
-    marked.InlineLexer = InlineLexer;
+    var options = store.get('Markdown') || {};
+
+    var defaults = merge(marked.defaults, options, {
+      renderer: Renderer
+    });
+
+    var lexer = Lexer(defaults);
+    marked.InlineLexer = InlineLexer(defaults);
+    // marked.setOptions(defaults);
     
     var parse = function(src) {
-      var tokens = Lexer.lex(src);
-      return marked.parser(tokens, Lexer.options);
+      var tokens = lexer.lex(src);
+      return marked.parser(tokens, lexer.options);
     }
+
+    window.ee.on('preferences.markdown.change', function(options) {
+      lexer = Lexer(options);
+      marked.InlineLexer = InlineLexer(options);
+
+      window.ee.emit('preferences.markdown.change.after');
+    });
 
     return parse;
 });
