356f6198631183c05d71c79ff134b1f8ab7851e5
rhiokim <rhio.kim@gmail.com>
2014-07-01 12:54:12 +0900

    implement new notifier

--- src/js/app/utils/NewsNotifier.js
+++ src/js/app/utils/NewsNotifier.js
@@ -5,6 +5,13 @@ define([], function() {
     url = manifest.app.news,
     currVersion = manifest.version;
 
+  var expire;
+  var newsData = store.get('_news') || {};
+
+  function setCookie() {
+
+  }
+
   function check(url) {
     var body = '';
 
@@ -17,6 +24,11 @@ define([], function() {
 
       res.on('end', function() {
         window.ee.emit('up.to.date.news', body);
+        
+        newsData.stime = new Date().getTime();
+        newsData.body = body;
+
+        store.set('_news', newsData);
       });
 
     }).on('error', function(e) {
@@ -25,6 +37,22 @@ define([], function() {
   }
 
   return function(news) {
-    check(news.url);
+    var nowTime = new Date().getTime();
+
+    if (!news || !news.url) {
+      return;
+    }
+
+    if (newsData.url != news.url) {
+      newsData.url = news.url;
+
+      check(news.url);
+    } else {
+      
+      /* time over */
+      if (newsData.stime + newsData.expire * 60 * 1000 > nowTime) {
+        window.ee.emit('up.to.date.news', newsData.body);
+      }
+    }
   }
 });
