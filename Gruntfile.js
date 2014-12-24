bb4623516c88e0e8e6ae8b3f7dfce9f5bacdae30
rhiokim <rhio.kim@gmail.com>
2014-12-25 01:14:30 +0900

    update node-webkit v0.11.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('./src/package.json'),
 
-    nwv: '0.11.3',
+    nwv: '0.11.4',
     os: platform(),
     arch: arch(),
     app: path.normalize('lib/node-webkit-v<%= nwv %>-<%= os %>-<%= arch %>'),
