b7bbe5c33d4af51bdd07d037746df3983dadf95e
rhiokim <rhio.kim@gmail.com>
2015-01-14 12:21:15 +0900

    remove pouchdb modules

--- src/node_modules/pouchdb/node_modules/pouchdb-mapreduce/evalfunc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function (func, emit, sum, log, isArray, toJSON) {
-  /*jshint evil:true,unused:false */
-  return eval("'use strict'; (" + func.replace(/;\s*$/, "") + ");");
-};
